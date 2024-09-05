a83e081431a326ec76bb3d86bdbe3df631c2bd3e
naedongdaeng2@gmail.com <naedongdaeng2@gmail.com>
2024-09-05 13:32:06 +0000

    Edited Articles, Home, Home.ItemTemplate1, Home.ItemTemplate2 and 3 more

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,3 +1,6 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 
 # This is a server module. It runs on the Anvil server,
@@ -11,7 +14,20 @@ import anvil.server
 # def say_hello(name):
 #   print("Hello, " + name + "!")
 #   return 42
-#
-@anvil.server.callable
-def get_articles():
-  return app_
+#https://mellow-blond-external.anvil.app/_/api/test_connection
+# @anvil.server.callable
+# def get_articles():
+#   return app_
+
+# @anvil.server.http_endpoint("test_connection", methods=["POST"])
+# def get_test_connection():
+#   return {"status": "Success", "message": "return-from-get"}
+
+@anvil.server.route('/add/:a/:b')
+def add_numbers(a, b):
+  a = int(a)
+  b = int(b)
+  return {
+    'originals': [a, b],
+    'sum': a + b,
+  }
