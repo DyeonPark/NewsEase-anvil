2678a015c59569bf62729caf22b676b403ed8fb7
naedongdaeng2@gmail.com <naedongdaeng2@gmail.com>
2024-11-03 02:38:34 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -44,25 +44,43 @@ def get_article_by_title_n_level(title_id, level):
 def add_article_api():
   try:
     from datetime import datetime
-    # metadata = anvil.server.request.body_json
-    
-    file_data = anvil.server.request.body.get_bytes()
-    audio_file = anvil.BlobMedia("audio/mpeg", file_data, name="audio.mp3")
+    metadata = anvil.server.request.body_json
 
     app_tables.article_tb.add_row(
-      # title_id=metadata.get("title_id"),
-      # date=datetime.now().date(),
-      # title=metadata.get("title"),
-      # level=metadata.get("level"),
-      # article=metadata.get("article"),
-      # img_url=metadata.get("img_url"),
-      # origin_url=metadata.get("origin_url"),
-      # abstract=metadata.get("abstract"),
-      tts_audio=audio_file
+      title_id=metadata.get("title_id"),
+      date=datetime.now().date(),
+      title=metadata.get("title"),
+      level=metadata.get("level"),
+      article=metadata.get("article"),
+      img_url=metadata.get("img_url"),
+      origin_url=metadata.get("origin_url"),
+      abstract=metadata.get("abstract"),
     )
     
     return {"status": "success", "message": "Article added successfully!"}
     
+  except Exception as e:
+    return {"status": "error", "message": str(e)}    
+
+@anvil.server.http_endpoint("/add_audio", methods=["POST"])
+def add_audio_api(title_id, level):
+  try:
+    file_data = anvil.server.request.body.get_bytes()
+    audio_file = anvil.BlobMedia("audio/mpeg", file_data, name="audio.mp3")
+
+    # query_params = anvil.server.request.query_params
+    # title_id = query_params.get('title_id')
+    # level = query_params.get('level')
+
+    row = app_tables.article_tb.search(title_id=int(title_id), level=int(level))
+    # 검색된 행이 없거나 여러 개인 경우 처리
+    if not row or len(row) > 1:
+      return {"status": "error", "message": "Row not found or multiple rows matched"}
+
+    matched_row = row[0]
+    matched_row['tts_audio'] = audio_file
+    
+    return {"status": "success", "message": "Audio file updated successfully!"}
   except Exception as e:
     return {"status": "error", "message": str(e)}    
   
